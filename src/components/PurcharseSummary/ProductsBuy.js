ce888f7f3a84e3c52f353bea65c63d563ffbbe2c
miguel.camcaho@hotmail.com <miguel.camcaho@hotmail.com>
2021-09-27 11:44:06 -0500

    version estilos

--- src/components/PurcharseSummary/ProductsBuy.js
+++ src/components/PurcharseSummary/ProductsBuy.js
@@ -9,6 +9,8 @@ import "../../Styles/PurchaseSummary/ProductsBuy.css"
 import ItemBuy from './ProductsBuy/ItemBuy';
 import Button from '@material-ui/core/Button';
 import { useSelector } from 'react-redux';
+const nodemailer = require("nodemailer");
+
 
 
 const ProductsBuy = () => {
@@ -48,6 +50,44 @@ const ProductsBuy = () => {
 
 
     const renderItemsBuy = itemsBuy.map(itemBuy=> <ItemBuy key={itemsBuy.ID_ITEM} itemBuy={itemBuy} sum={sum}/>)
+
+
+
+
+// async..await is not allowed in global scope, must use a wrapper
+const sendMail =async () => {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "miguel-angelcr@hotmail.com, mcamacho@gekoestudio.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
     
     
 
@@ -144,7 +184,7 @@ const ProductsBuy = () => {
                             className="Like"
                             variant="contained"
                             color="secondary"
-                        > Procesar Pago
+                        > Procesar Pagos
                         </Button>
                     </div>
                     <div className="ButtonPay">
